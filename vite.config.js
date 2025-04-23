ba6ceec36a90797e36112674bc60b41923c660b4
Bishoy73 <bishoy.effat7373@gmail.com>
2025-04-24 00:21:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Export the configuration
 export default defineConfig({
   plugins: [react()],
-})
-
+  server: {
+    port: 3000,  // Change to another port if needed
+    host: '0.0.0.0',  // Ensures the server is accessible externally
+  },
+});
